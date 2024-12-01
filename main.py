4dc034f08bf37ec307d07470e482d7ea0f3b8cfb
Pndu <121600932+Pndu30@users.noreply.github.com>
2024-12-01 18:07:26 +1100

    Testings

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from models.HarDNet import HarDNet
-
+from models.HarDUNet import HarDUNet
+from data.torchio_dataset import get_isles_22
+from utils.train_utils import hardunet_train_loop
+from torch import nn
+from torch.nn import functional as F
+from torch import optim
 
 if __name__ == "__main__":  # pragma: no cover
-    model = HarDNet(arch="39DS")
-    print(len(model.layers))
-    model2 = HarDNet(arch="68")
-    print(len(model2.layers))
+    data = get_isles_22(batch_size=3, shuffle=True, sample_data=False)
+    print(len(data))
+    for inputs, targets in data:
+        print(f"Input shape: {inputs.shape}")
+        print(f"Target shape: {targets.shape}")
+        break
+
+    # model = HarDUNet(arch='39DS')
+
+    # optimizer = optim.AdamW(lr=0.1, params=model.parameters())
+    # loss_fn = F.binary_cross_entropy()
+
+    
